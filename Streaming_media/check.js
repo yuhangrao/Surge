8085e88b8ddc3ba4d43863d29c2dce04b24cf3fa
yuhang rao <jarvis2593227447@gmail.com>
2022-01-17 20:01:08 +0800

    Create check.js

---
+++ Streaming_media/check.js
@@ -0,0 +1,288 @@
+/***
+
+Thanks to & modified from 
+1. https://gist.githubusercontent.com/Hyseen/b06e911a41036ebc36acf04ddebe7b9a/raw/nf_check.js
+2. https://github.com/AtlantisGawrGura/Quantumult-X-Scripts/blob/main/media.js
+3. https://github.com/CoiaPrant/MediaUnlock_Test/blob/main/check.sh
+4. https://raw.githubusercontent.com/KOP-XIAO/QuantumultX/master/Scripts/streaming-ui-check.js
+
+For Loon 344+ ONLY!!
+
+[script]
+
+generic script-path= https://raw.githubusercontent.com/Tartarus2014/Script/master/stream-ui-check.js, tag=流媒体-解锁查询, img-url=checkmark.seal.system, enabled=true
+
+@XIAO_KOP
+
+**/
+
+const BASE_URL = 'https://www.netflix.com/title/';
+const BASE_URL_DISNEY = 'https://www.disneyplus.com';
+const FILM_ID = 81215567
+
+const link = { "media-url": "https://raw.githubusercontent.com/KOP-XIAO/QuantumultX/master/img/southpark/7.png" } 
+const policy_name = "Netflix" //填入你的 netflix 策略组名
+
+const arrow = " ➟ "
+
+const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36'
+
+// 即将登陆
+const STATUS_COMING = 2
+// 支持解锁
+const STATUS_AVAILABLE = 1
+// 不支持解锁
+const STATUS_NOT_AVAILABLE = 0
+// 检测超时
+const STATUS_TIMEOUT = -1
+// 检测异常
+const STATUS_ERROR = -2
+
+var inputParams = $environment.params;
+var nodeName = inputParams.node;
+
+var opts = {
+    node: nodeName,
+};
+
+var opts1 = {
+  node: nodeName,
+  redirection: false
+};
+
+var flags = new Map([[ "AC" , "🇦🇨" ] ,["AE","🇦🇪"], [ "AF" , "🇦🇫" ] , [ "AI" , "🇦🇮" ] , [ "AL" , "🇦🇱" ] , [ "AM" , "🇦🇲" ] , [ "AQ" , "🇦🇶" ] , [ "AR" , "🇦🇷" ] , [ "AS" , "🇦🇸" ] , [ "AT" , "🇦🇹" ] , [ "AU" , "🇦🇺" ] , [ "AW" , "🇦🇼" ] , [ "AX" , "🇦🇽" ] , [ "AZ" , "🇦🇿" ] , ["BA", "🇧🇦"], [ "BB" , "🇧🇧" ] , [ "BD" , "🇧🇩" ] , [ "BE" , "🇧🇪" ] , [ "BF" , "🇧🇫" ] , [ "BG" , "🇧🇬" ] , [ "BH" , "🇧🇭" ] , [ "BI" , "🇧🇮" ] , [ "BJ" , "🇧🇯" ] , [ "BM" , "🇧🇲" ] , [ "BN" , "🇧🇳" ] , [ "BO" , "🇧🇴" ] , [ "BR" , "🇧🇷" ] , [ "BS" , "🇧🇸" ] , [ "BT" , "🇧🇹" ] , [ "BV" , "🇧🇻" ] , [ "BW" , "🇧🇼" ] , [ "BY" , "🇧🇾" ] , [ "BZ" , "🇧🇿" ] , [ "CA" , "🇨🇦" ] , [ "CF" , "🇨🇫" ] , [ "CH" , "🇨🇭" ] , [ "CK" , "🇨🇰" ] , [ "CL" , "🇨🇱" ] , [ "CM" , "🇨🇲" ] , [ "CN" , "🇨🇳" ] , [ "CO" , "🇨🇴" ] , [ "CP" , "🇨🇵" ] , [ "CR" , "🇨🇷" ] , [ "CU" , "🇨🇺" ] , [ "CV" , "🇨🇻" ] , [ "CW" , "🇨🇼" ] , [ "CX" , "🇨🇽" ] , [ "CY" , "🇨🇾" ] , [ "CZ" , "🇨🇿" ] , [ "DE" , "🇩🇪" ] , [ "DG" , "🇩🇬" ] , [ "DJ" , "🇩🇯" ] , [ "DK" , "🇩🇰" ] , [ "DM" , "🇩🇲" ] , [ "DO" , "🇩🇴" ] , [ "DZ" , "🇩🇿" ] , [ "EA" , "🇪🇦" ] , [ "EC" , "🇪🇨" ] , [ "EE" , "🇪🇪" ] , [ "EG" , "🇪🇬" ] , [ "EH" , "🇪🇭" ] , [ "ER" , "🇪🇷" ] , [ "ES" , "🇪🇸" ] , [ "ET" , "🇪🇹" ] , [ "EU" , "🇪🇺" ] , [ "FI" , "🇫🇮" ] , [ "FJ" , "🇫🇯" ] , [ "FK" , "🇫🇰" ] , [ "FM" , "🇫🇲" ] , [ "FO" , "🇫�" ] , [ "FR" , "🇫🇷" ] , [ "GA" , "🇬🇦" ] , [ "GB" , "🇬🇧" ] , [ "HK" , "🇭🇰" ] ,["HU","🇭🇺"], [ "ID" , "🇮🇩" ] , [ "IE" , "🇮🇪" ] , [ "IL" , "🇮🇱" ] , [ "IM" , "🇮🇲" ] , [ "IN" , "🇮🇳" ] , [ "IS" , "🇮🇸" ] , [ "IT" , "🇮🇹" ] , [ "JP" , "🇯🇵" ] , [ "KR" , "🇰🇷" ] , [ "LU" , "🇱🇺" ] , [ "MO" , "🇲🇴" ] , [ "MX" , "🇲🇽" ] , [ "MY" , "🇲🇾" ] , [ "NL" , "🇳🇱" ] , [ "PH" , "🇵🇭" ] , [ "RO" , "🇷🇴" ] , [ "RS" , "🇷🇸" ] , [ "RU" , "🇷🇺" ] , [ "RW" , "🇷🇼" ] , [ "SA" , "🇸🇦" ] , [ "SB" , "��🇧" ] , [ "SC" , "🇸🇨" ] , [ "SD" , "🇸🇩" ] , [ "SE" , "🇸🇪" ] , [ "SG" , "🇸🇬" ] , [ "TH" , "🇹🇭" ] , [ "TN" , "🇹🇳" ] , [ "TO" , "🇹🇴" ] , [ "TR" , "🇹🇷" ] , [ "TV" , "🇹🇻" ] , [ "TW" , "🇨🇳" ] , [ "UK" , "🇬🇧" ] , [ "UM" , "🇺🇲" ] , [ "US" , "🇺🇸" ] , [ "UY" , "🇺🇾" ] , [ "UZ" , "🇺🇿" ] , [ "VA" , "🇻🇦" ] , [ "VE" , "🇻🇪" ] , [ "VG" , "🇻🇬" ] , [ "VI" , "🇻🇮" ] , [ "VN" , "🇻🇳" ] , [ "ZA" , "🇿🇦"]])
+
+let result = {
+  "title": '    📺  流媒体服务查询',
+  "Netflix": '<b>Netflix: </b>检测失败，请重试 ❗️',
+  "Disney+": "<b>Disney+: </b>检测失败，请重试 ❗️",
+  //"Google": "Google 定位: 检测失败，请重试"
+
+}
+// const message = {
+//   action: "get_policy_state",
+//   content: $environment.params
+// };
+
+;(async () => {
+  //testYTB()
+  //testDazn()
+  //testParam()
+  let [{ region, status }] = await Promise.all([testDisneyPlus(),testNf(FILM_ID)])
+  console.log(result["Netflix"])
+  console.log(`testDisneyPlus: region=${region}, status=${status}`)
+  if (status==STATUS_COMING) {
+    //console.log(1)
+    result["Disney"] = "<b>Disneyᐩ:</b> ⚠️ 即将登陆 ➟ "+'⟦'+flags.get(region.toUpperCase())+"⟧"
+  } else if (status==STATUS_AVAILABLE){
+    //console.log(2)
+    result["Disney"] = "<b>Disneyᐩ:</b> 支持 ➟ "+'⟦'+flags.get(region.toUpperCase())+"⟧ 🎉"
+    console.log(result["Disney"])
+  } else if (status==STATUS_NOT_AVAILABLE) {
+    //console.log(3)
+    result["Disney"] = "<b>Disneyᐩ:</b> 未支持 🚫 "
+  } else if (status==STATUS_TIMEOUT) {
+    result["Disney"] = "<b>Disneyᐩ:</b> 检测超时 🚦 "
+  }
+
+  let content = "--------------------------------------</br>"+([result["Disney"],result["Netflix"]]).join("</br></br>")
+  content = content + "</br>--------------------------------------</br>"+"<font color=#CD5C5C>"+"<b>节点</b> ➟ " + nodeName+ "</font>"
+  content =`<p style="text-align: center; font-family: -apple-system; font-size: large; font-weight: thin">` + content + `</p>`
+    // $notify(typeof(output),output)
+  // console.log("done---------------------");
+  console.log(content);
+  $done({"title":result["title"],"htmlMessage":content})
+
+})()
+.finally(() => {
+    $done({"title":result["title"],"htmlMessage":`<p style="text-align: center; font-family: -apple-system; font-size: large; font-weight: thin">`+'----------------------</br></br>'+"🚥 检测异常"+'</br></br>----------------------</br>'+ nodeName + `</p>`})
+});
+
+
+async function testDisneyPlus() {
+  try {
+    let { region, cnbl } = await Promise.race([testHomePage(), timeout(7000)])
+    console.log(`homepage: region=${region}, cnbl=${cnbl}`)
+    // 即将登陆
+//  if (cnbl == 2) {
+//    return { region, status: STATUS_COMING }
+//  }
+    let { countryCode, inSupportedLocation } = await Promise.race([getLocationInfo(), timeout(7000)])
+    console.log(`getLocationInfo: countryCode=${countryCode}, inSupportedLocation=${inSupportedLocation}`)
+    
+    region = countryCode ?? region
+    console.log( "region:"+region)
+    // 即将登陆
+    if (inSupportedLocation === false || inSupportedLocation === 'false') {
+      return { region, status: STATUS_COMING }
+    } else {
+      // 支持解锁
+      return { region, status: STATUS_AVAILABLE }
+    }
+    
+  } catch (error) {
+    console.log("error:"+error)
+    
+    // 不支持解锁
+    if (error === 'Not Available') {
+      console.log("不支持")
+      return { status: STATUS_NOT_AVAILABLE }
+    }
+    
+    // 检测超时
+    if (error === 'Timeout') {
+      return { status: STATUS_TIMEOUT }
+    }
+    
+    return { status: STATUS_ERROR }
+  } 
+  
+}
+
+function getLocationInfo() {
+  return new Promise((resolve, reject) => {
+    let opts0 = {
+      url: 'https://disney.api.edge.bamgrid.com/graph/v1/device/graphql',
+      node: nodeName,
+      headers: {
+        'Accept-Language': 'en',
+        "Authorization": 'ZGlzbmV5JmJyb3dzZXImMS4wLjA.Cu56AgSfBTDag5NiRA81oLHkDZfu5L3CKadnefEAY84',
+        'Content-Type': 'application/json',
+        'User-Agent': UA,
+      },
+      body: JSON.stringify({
+        query: 'mutation registerDevice($input: RegisterDeviceInput!) { registerDevice(registerDevice: $input) { grant { grantType assertion } } }',
+        variables: {
+          input: {
+            applicationRuntime: 'chrome',
+            attributes: {
+              browserName: 'chrome',
+              browserVersion: '94.0.4606',
+              manufacturer: 'microsoft',
+              model: null,
+              operatingSystem: 'windows',
+              operatingSystemVersion: '10.0',
+              osDeviceIds: [],
+            },
+            deviceFamily: 'browser',
+            deviceLanguage: 'en',
+            deviceProfile: 'windows',
+          },
+        },
+      }),
+    }
+
+    $httpClient.post(opts0, (error, response, data) => {
+        if (error) {
+            reject(error)
+            return
+        }
+        if (response.status !== 200) {
+            console.log('getLocationInfo: ' + data)
+            reject('Not Available')
+        } else {
+            let {
+                inSupportedLocation,
+                location: { countryCode },
+              } = JSON.parse(data)?.extensions?.sdk?.session
+            resolve({ inSupportedLocation, countryCode })
+        }
+    });
+  })
+}
+
+function testHomePage() {
+  return new Promise((resolve, reject) => {
+    let opts0 = {
+      url: 'https://www.disneyplus.com/',
+      node: nodeName,
+      headers: {
+        'Accept-Language': 'en',
+        'User-Agent': UA,
+      },
+    }
+    $httpClient.get(opts0, (error, response, data) => {
+        if (error) {
+            reject(error)
+            return
+        }
+        if (response.status !== 200 || data.indexOf('unavailable') !== -1) {
+            reject('Not Available')
+            return
+        } else {
+            let match = data.match(/Region: ([A-Za-z]{2})[\s\S]*?CNBL: ([12])/)
+            if (!match) {
+            resolve({ region: '', cnbl: '' })
+            return
+            } else {
+            let region = match[1]
+            let cnbl = match[2]
+            //console.log("homepage"+region+cnbl)
+            resolve({ region, cnbl })
+            }
+        }
+    });
+  })
+}
+
+function timeout(delay = 5000) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject('Timeout')
+    }, delay)
+  })
+}
+
+function testNf(filmId) {
+  return new Promise((resolve, reject) =>{
+    let option = {
+      url: BASE_URL + filmId,
+      node: nodeName,
+      timeout: 3200,
+      headers: {
+        'User-Agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36',
+      },
+    }
+
+    $httpClient.get(option, (error, response, data) => {
+        if (error) {
+            result["Netflix"] = "<b>Netflix: </b>检测超时 🚦"
+            console.log(result["Netflix"])
+            resolve("timeout")
+            return
+        }
+
+        console.log("nf:"+response.status)
+        if (response.status === 404) {
+            result["Netflix"] = "<b>Netflix: </b>支持自制剧集 ⚠️"
+            console.log("nf:"+result["Netflix"])
+            resolve('Not Found')
+            return 
+        } else if (response.status === 403) {
+            //console.log("nfnf")
+            result["Netflix"] = "<b>Netflix: </b>未支持 🚫"
+            console.log("nf:"+result["Netflix"])
+            //$notify("nf:"+result["Netflix"])
+            resolve('Not Available')
+            return
+        } else if (response.status === 200) {
+            let url = response.headers["XOriginatingURL"]
+            let region = 'us'
+            if (url != undefined) {
+              region = url.split('/')[3]
+              region = region.split('-')[0]
+              if (region == 'title') {
+                region = 'us'
+              }
+            }
+            console.log("nf:"+region)
+            result["Netflix"] = "<b>Netflix: </b>完整支持"+arrow+ "⟦"+flags.get(region.toUpperCase())+"⟧ 🎉"
+            //$notify("nf:"+result["Netflix"])
+            resolve("nf:"+result["Netflix"])
+            return 
+        }
+    });
+  }
+  )
+}
