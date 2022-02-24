745e0422bfc7adfba0cb1e33cca6f3dc6c4932bf
yuhang rao <yuhangrao@users.noreply.github.com>
2022-02-24 23:35:59 +0800

    Bug fix

--- Module/Panel/Dler_sub_info.js
+++ Module/Panel/Dler_sub_info.js
@@ -112,7 +112,7 @@ function formatTime(time) {
   let year = dateObj.getFullYear();
   let month = dateObj.getMonth() + 1;
   let day = dateObj.getDate();
-  if(month.length===1) month = "0" + month;
-  if(day.length===) day = "0" + day;
+  if(month.length === 1) month = "0" + month;
+  if(day.length === 1) day = "0" + day;
   return year + "-" + month + "-" + day;
 }
