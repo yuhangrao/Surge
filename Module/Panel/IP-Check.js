9e3657442a44c06b99e1e138e69e7119890cdcd1
yuhang rao <yuhangrao@users.noreply.github.com>
2022-05-23 09:49:42 +0800

    Rename

---
+++ Module/Panel/IP-Check.js
@@ -0,0 +1,31 @@
+/*
+ * 节点信息面板
+ * @author： yuhang rao
+ * Thanks: congcong0806
+ */
+ 
+let url = "http://ip-api.com/json"
+
+$httpClient.get(url, function(error, response, data){
+    let jsonData = JSON.parse(data)
+    let ip = jsonData.query
+    let country = jsonData.country
+    let emoji = getFlagEmoji(jsonData.countryCode)
+    let city = jsonData.city
+    let isp = jsonData.isp
+  body = {
+    title: "Node Info",
+    content: `IP: ${ip}\nISP: ${isp}\nLocation: ${emoji} ${country} - ${city}`,
+    icon: "personalhotspot.circle.fill"
+  }
+  $done(body);
+});
+
+
+function getFlagEmoji(countryCode) {
+    const codePoints = countryCode
+      .toUpperCase()
+      .split('')
+      .map(char =>  127397 + char.charCodeAt());
+    return String.fromCodePoint(...codePoints);
+}
