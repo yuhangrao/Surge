3f8b810571290859a5e0914e9a4449e8d4de7237
yuhang rao <yuhangrao@users.noreply.github.com>
2022-08-08 05:17:33 +0800

    Bug fix

--- Module/Bilibili/Bilibili-Proto-iPad.js
+++ Module/Bilibili/Bilibili-Proto-iPad.js
@@ -44,6 +44,7 @@ if(url.includes("Dynamic/DynAll") && method === postMethod){
 
     if(!dynAllReplyMessage.hasOwnProperty('upList') || dynAllReplyMessage.upList === null){
         console.log('upList为空');
+        }
     }
 
     if(!dynAllReplyMessage.dynamicList.hasOwnProperty('list') || dynAllReplyMessage.dynamicList.list === null || dynAllReplyMessage.dynamicList.list.length === 0){
