c0d1dd736dadc9c957168f20572c04bf9d81e2f1
yuhang rao <yuhangrao@users.noreply.github.com>
2022-08-12 08:50:50 +0800

    Change icon

--- Module/Panel/Dler-Sub-Info.js
+++ Module/Panel/Dler-Sub-Info.js
@@ -26,8 +26,8 @@
   $done({
     title: `${args.title}`,
     content: content.join("\n"),
-    icon: args.icon || "airplane.circle",
-    "icon-color": args.color || "#007aff",
+    icon: "cloud",
+//    "icon-color": args.color || "#007aff",
   });
 })();
 
